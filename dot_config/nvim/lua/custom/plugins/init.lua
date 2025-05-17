cdc20ceb112b30a1ae91bc922498d03e3ddae7eb
Billy Priambodo <edo.billy@gmail.com>
2025-05-17 08:56:36 +0700

    feat: add go

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -155,6 +155,26 @@ return {
   {
     'tpope/vim-fugitive',
   },
+  {
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('go').setup()
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
+    'johmsalas/text-case.nvim',
+    config = function()
+      require('textcase').setup {}
+    end,
+  },
   -- {
   --   'mistweaverco/kulala.nvim',
   --   config = function()
